90017ad5c81a6679e07ebf602c4afb5547abb6ba
Maya <145165822+MayaChen350@users.noreply.github.com>
2026-01-02 15:52:09 -0500

    Setting up svelte

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import tailwindcss from "@tailwindcss/vite";
+
+const veryOldDeviceInUse = 'ios_saf >= 10'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [tailwindcss(), sveltekit(),
+        legacy({
+            targets: ['defaults', veryOldDeviceInUse],
+            renderLegacyChunks: false,
+            modernPolyfills: true
+        }),
+    ]
 });
