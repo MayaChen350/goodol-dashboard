9fb35e02a340af01f8c1574379429d69a547949d
Maya <maychennn@protonmail.com>
2026-05-22 19:38:32 -0400

    Revert "Setting up svelte"
    This reverts commit 90017ad5c81a6679e07ebf602c4afb5547abb6ba.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,6 @@
-import {sveltekit} from '@sveltejs/kit/vite';
-import {defineConfig} from 'vite';
-import legacy from '@vitejs/plugin-legacy';
-import tailwindcss from "@tailwindcss/vite";
-
-const veryOldDeviceInUse = 'ios_saf >= 10'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [tailwindcss(), sveltekit(),
-        legacy({
-            targets: ['defaults', veryOldDeviceInUse],
-            renderLegacyChunks: false,
-            modernPolyfills: true
-        }),
-    ]
+	plugins: [sveltekit()]
 });
